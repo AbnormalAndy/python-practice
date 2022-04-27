65743b4f8d79e8209280dbca938342e6cdb4a492
Jinx <jinxjilted@gmail.com>
2022-04-26 18:07:41 -0700

    Add comments to clarify code

--- Python/SecretAuction/SecretAuction.py
+++ Python/SecretAuction/SecretAuction.py
@@ -2,25 +2,32 @@ import SecretAuctionArt
 
 print(SecretAuctionArt.logo)
 
-# Ask for Name Input
-# Ask for Bid Price
-# Add Name and Bid into Dictionary - Key / Value
-# Ask if there are other users who would like to bid.
 # Yes -> Clear Screen -> Restart Loop
 # No -> Find the Highest Bidder -> Declare them the Winner
 
+# Empty dictionary for the auction.
 auction_dictionary = {}
+
+# Function to add bidder and respective bid price.
 def auction(bidder, price):
   auction_dictionary[bidder] = price
 
 end_auction = False
 while not end_auction:
 
+  # Ask for Name Input
   bidder_name = input("What is your name? ").lower()
+ 
+  # Ask for Bid Price
   bid_price = int(input("What is your bid? "))
+
+  # Add Name and Bid into Dictionary - Key / Value
   auction(bidder_name, bid_price)
+
+  # Ask if there are other users who would like to bid.
   continue_auction = input("Are there any others who would like to bid?\nYes (Y) - No (N)\n").lower()
   if continue_auction == 'no' or continue_auction == 'n':
+    # Finds the highest bidder.
     name = ''
     bid_price = 0
     for bidder in auction_dictionary:
@@ -29,11 +36,12 @@ while not end_auction:
         bid_price = bid
         name = bidder
     # clear()
+    # print(auction_dictionary)
     print("End of the auction.")
     print(f"The winner of the auction is {name.capitalize()} with a bid price of ${bid_price}.")
-    print(auction_dictionary)
     end_auction = True
   else:
+    # Adds another bidder.
     # clear()
+    # print(auction_dictionary)
     print("Please add the next bidder.")
-    print(auction_dictionary)
