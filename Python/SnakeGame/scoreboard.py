06f709fd796055422851af312988235dd8018db2
Jinx <jinxjilted@gmail.com>
2022-06-05 16:31:38 -0700

    Add high score permanence

--- Python/SnakeGame/scoreboard.py
+++ Python/SnakeGame/scoreboard.py
@@ -5,6 +5,12 @@ ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
 
+# with open("high_score.txt", mode="w") as file:
+#   contents = file.read()
+
+
+
+
 class Scoreboard(Turtle):
     
     def __init__(self):
@@ -14,14 +20,21 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(0, 270)
         self.score = 0
+        with open("high_score.txt") as high_score:
+            self.high_score = int(high_score.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode="w") as high_score:
+                high_score.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
