8a59155043736da1f04efd689875300d5d059158
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-13 10:20:59 -0600

    Add 100 Days of Python files.

---
+++ 100DaysOfCodeProjects/day-1/bandnamegenerator.py
@@ -0,0 +1,8 @@
+print("Welcome to Band Name Generator")
+
+cityName = input("What is the name of the city you grew up in?\n")
+
+petName = input("What is the name of your pet?\n")
+
+print(f"Your band name could be {cityName.capitalize()} {petName.capitalize()}.")
+
