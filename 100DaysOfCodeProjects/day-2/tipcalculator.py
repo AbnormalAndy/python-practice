957ce3c94b66ed4d170198bcd476b78c8da4f000
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-13 10:45:23 -0600

    Add tipcalculator.py code.

--- 100DaysOfCodeProjects/day-2/tipcalculator.py
+++ 100DaysOfCodeProjects/day-2/tipcalculator.py
@@ -1,3 +1,15 @@
-print("Welcome to the tip calculator!")
+print("\nWelcome to the tip calculator!")
+
+totalBill = float(input("What is the total bill?\n"))
+
+tipAmount = float(input("How much tip would you like to give? 10, 12, or 15?\n"))
+
+tipAmount = 1 + (tipAmount / 100)
+
+numberOfPeople = float(input("How many people to split the bill?\n"))
+
+payPerPerson = (totalBill * tipAmount) / numberOfPeople
+
+print(f"Each person should pay: ${payPerPerson:.2f}.\n")
 
 
