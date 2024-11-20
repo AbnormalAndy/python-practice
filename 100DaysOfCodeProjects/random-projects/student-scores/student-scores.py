e10394999284c0907f04b48fd98a409a397988fd
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-20 11:49:29 -0600

    Add student-scores.py project.

---
+++ 100DaysOfCodeProjects/random-projects/student-scores/student-scores.py
@@ -0,0 +1,24 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+
+student_grades = {}
+
+
+for score in student_scores:
+    if student_scores[score] > 90:
+        student_grades[score] = "Outstanding"
+    elif student_scores[score] > 80:
+        student_grades[score] = "Exceeds Expectations"
+    elif student_scores[score] > 70:
+        student_grades[score] = "Acceptable"
+    else:
+        student_grades[score] = "Fail"
+        
+
+print(student_grades)
