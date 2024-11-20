e6ba0ba063cd3f64064e10cdf4125213a823bc46
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-20 10:14:14 -0600

    Refactor caesar-cipher.py file.

--- 100DaysOfCodeProjects/day-8/caesar-cipher.py
+++ 100DaysOfCodeProjects/day-8/caesar-cipher.py
@@ -1,40 +1,54 @@
 list_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-user_decision = input("\"Encode\" or \"Decode\"\n")
+print("\nWelcome to the CAESAR CIPHER!\n")
 
 
-if user_decision.lower() == "encode":
-    shift = int(input("How many letters would you like to shift?\n"))
-    phrase = input("What would you like to encode?\n")
+end_program = False
+
+
+while end_program != True:
     secret_cipher = ""
 
-    for letter in phrase:
-        if letter == " ":
-            secret_cipher += " "
-        else:
-            total = list_alphabet.index(letter) + shift
-            if total > 25:
-                total = total - 26
+
+    user_decision = input("Choose: \"Encode\" or \"Decode\"\n")
+
+
+    if user_decision.lower() == "encode" or user_decision.lower() == "decode":
+        phrase = input("\nWhat is to be encoded or decoded?\n")
+        shift = int(input("\nHow many letters is the shift?\n"))
+
+
+        for letter in phrase:
+            if letter == " ":
+                secret_cipher += " "
+            if user_decision.lower() == "encode":
+                total = list_alphabet.index(letter) + shift
+                if total > 25:
+                    total = total - 26
+            if user_decision.lower() == "decode":
+                total = list_alphabet.index(letter) - shift
+                if total < -25:
+                    total = total + 26
             secret_cipher += list_alphabet[total]
 
-    print(secret_cipher)
 
 
-if user_decision.lower() == "decode":
-    shift = int(input("How many letters would you like to shift?\n"))
-    phrase = input("What would you like to decode?\n")
-    secret_cipher = ""
+        print(f"\nSecret Cipher: {secret_cipher}")
 
-    for letter in phrase:
-        if letter == " ":
-            secret_cipher += " "
+
+        restart_decision = input("\nWould you like to restart? \"Yes\" or \"No\"\n")
+        
+
+        if restart_decision.lower() != "yes":
+            print("\nThank you for using the Caesar Cipher!\n")
+            end_program = True
         else:
-            total = list_alphabet.index(letter) - shift
-            if total < -25:
-                total = total + 26
-            secret_cipher += list_alphabet[total]
+            print("\nPlease choose one of the following choices.")
 
-    print(secret_cipher)
+
+    else:
+        print("\nInvalid choice. Please try again!.")
+        print("Please choose one of the following choices.")
 
 
