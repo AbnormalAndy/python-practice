1ee9ec82c0c63a01ae1004e4184d8b9b41d5198f
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-22 11:10:27 -0600

    Begin calculator.py project.

---
+++ 100DaysOfCodeProjects/day-10/calculator.py
@@ -0,0 +1,34 @@
+
+
+def addition(number_one, number_two):
+    return float(number_one + number_two)
+
+
+def subtraction(number_one, number_two):
+    return float(number_one - number_two)
+
+
+def multiplication(number_one, number_two):
+    return float(number_one * number_two)
+
+
+def division(number_one, number_two):
+    return float(number_one / number_two)
+
+
+number1 = 15
+number2 = 3
+
+
+print(addition(number1, number2))
+print(subtraction(number1, number2))
+print(multiplication(number1, number2))
+print(division(number1, number2))
+
+
+# Calculator
+# What's the first number?
+# Pick an operation? + - * /
+# What's the next number?
+
+
