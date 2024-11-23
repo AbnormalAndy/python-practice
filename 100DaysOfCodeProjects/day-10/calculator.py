650b589cef8035ba8c7934d76b195453464f1432
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-22 19:30:55 -0600

    Update calculator.py project.

--- 100DaysOfCodeProjects/day-10/calculator.py
+++ 100DaysOfCodeProjects/day-10/calculator.py
@@ -1,4 +1,4 @@
-print("Welcome to the CALCULATOR!")
+print("\nWelcome to the CALCULATOR!\n")
 
 
 def addition(number_one, number_two):
@@ -31,21 +31,42 @@ def calculation(operator, number_one, number_two):
             return False
 
 
-number1 = 15
-number2 = 3
+end_calculator = False
+end_calculation = False
 
 
-print(calculation("+", number1, number2))
-print(calculation("-", number1, number2))
-print(calculation("*", number1, number2))
-print(calculation("/", number1, number2))
-print(calculation("", number1, number2))
+while end_calculator != True:
+    total = 0
+    end_calculation = False
 
 
-# Calculator
-# What's the first number?
-# Pick an operation? + - * /
-# What's the next number?
-# Return FALSE should repeat previous numbers.
+    first_number = int(input("\nWhat is the first number?\n"))
+    operator_choice = input("\n+\n-\n*\n/\nPick an operation.\n")
+    next_number = int(input("\nWhat is the next number?\n"))
+    total = calculation(operator_choice, first_number, next_number)
+
+    
+    print(f"\n{first_number:.1f} {operator_choice} {next_number:.1f} = {total:.1f}\n")
+
+
+    while end_calculation != True:
+        print("Type 'q' to quit the calculator.")
+        continue_calculating = input(f"Type 'y' to continue calculating with {total:.1f}, or type 'n' to start a new calculation.\n")
+
+
+        if continue_calculating.lower() == 'n':
+            end_calculation = True
+        elif continue_calculating.lower() == 'y':
+            first_number = total
+            operator_choice = input("\n+\n-\n*\n/\nPick an operation.\n")
+            next_number = int(input("\nWhat is the next number?\n"))
+            total = calculation(operator_choice, first_number, next_number)
+            print(f"\n{first_number:.1f} {operator_choice} {next_number:.1f} = {total:.1f}\n")
+        else:
+            end_calculator = True
+            end_calculation = True
+
+
+print("\nThanks for using the CALCULATOR!\n")
 
 
