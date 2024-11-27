0db98d3ce1403a1b21ec60459ef3f2e06f37ac9d
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-26 21:58:46 -0600

    Update higher-lower-game.py code to choose users and compare them.

--- 100DaysOfCodeProjects/day-14/higher_lower_game.py
+++ 100DaysOfCodeProjects/day-14/higher_lower_game.py
@@ -5,19 +5,26 @@ import game_data
 print("HIGHER or LOWER")
 
 
-print(game_data.data[0])
-print(game_data.data[0]["name"])
-print(game_data.data[0]["follower_count"])
-print(game_data.data[0]["description"])
-print(game_data.data[0]["country"])
+# Could get the same Instagram user.
+# TO-DO: Choose until not the same.
+instagram_a = random.choice((game_data.data))
+instagram_b = random.choice((game_data.data))
+
+
+print(f"Compare A: {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.")
+print(f"Follower Count: {instagram_a['follower_count']}.")
+print(f"Compare B: {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.")
+print(f"Follower Count: {instagram_b['follower_count']}.")
+
+
+if instagram_a['follower_count'] > instagram_b['follower_count']:
+    print(f"More Followers ({instagram_a['follower_count']}): {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.")
+else:
+    print(f"More Followers ({instagram_b['follower_count']}): {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.")
 
 
 # TO-DO:
-# - More followers on instagram.
-# - Compare Instagram User A to Instagram User B
-#     - Compare follwer numbers.
-#     - Description of Instagram user.
-# - "Who has more followers? Type 'A' or 'B'
+# - Ask who has more followers on instagram.
 # - Continues until wrong answer.
 # - Display Score
 # - End game when wrong answer.
