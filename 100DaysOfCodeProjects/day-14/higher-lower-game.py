e1c74d227849365e4b5e864dda879b565eb70a15
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-26 21:06:45 -0600

    Begin higher-lower-game.py project.

---
+++ 100DaysOfCodeProjects/day-14/higher-lower-game.py
@@ -0,0 +1,18 @@
+import random
+
+
+print("HIGHER or LOWER")
+
+
+
+# TO-DO:
+# - More followers on instagram.
+# - Compare Instagram User A to Instagram User B
+#     - Compare follwer numbers.
+#     - Description of Instagram user.
+# - "Who has more followers? Type 'A' or 'B'
+# - Continues until wrong answer.
+# - Display Score
+# - End game when wrong answer.
+
+
