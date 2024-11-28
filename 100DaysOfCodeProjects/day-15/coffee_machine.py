62dc66f770a725bae304ac52bd236e97125e4120
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-28 11:41:49 -0600

    Begin coffee_machine.py project.

---
+++ 100DaysOfCodeProjects/day-15/coffee_machine.py
@@ -0,0 +1,20 @@
+
+
+# TO-DO
+# - 3 Hot Flavors
+# - Coins Operate
+# - Automatic Cup Dispenser
+# - Counting Cup Selling
+
+# - Water Inlet
+# - Coin Outlet
+# - Coin Acceptor
+# - LCD Display
+#     - Drink 1
+#     - Drink 2
+#     - Drink 3
+# - Menu
+# - Drink Outlet
+# - Waste Water Box
+
+
