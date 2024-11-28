a174e038005a78594c7906526e9ccbe4727f6aa1
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-28 12:03:00 -0600

    Update coffee_machine.py project.

--- 100DaysOfCodeProjects/day-15/coffee_machine.py
+++ 100DaysOfCodeProjects/day-15/coffee_machine.py
@@ -1,11 +1,38 @@
 
 
 # TO-DO
+# Key Points
 # - 3 Hot Flavors
+#     - Espresso ($1.50)
+#         - 50 mL Water
+#         - 18 g Coffee
+#     - Latte ($2.50)
+#         - 200 mL Water
+#         - 24 g Coffee
+#         - 150 mL Milk
+#     - Cappuccino ($3.00)
+#         - 250 mL Water
+#         - 24 g Coffee
+#         - 100 mL Milk
 # - Coins Operate
+#     - How many pennies?
+#         - Peny (0.01)
+#     - How many nickels?
+#         - Nickel (0.05)
+#     - How many dimes?
+#         - Dime (0.10)
+#     - How many quarters?
+#         - Quarter (0.25)
 # - Automatic Cup Dispenser
 # - Counting Cup Selling
 
+
+# Water (300 mL)
+# Milk (200 mL)
+# Coffee (100 g)
+
+
+# Analytic Table
 # - Water Inlet
 # - Coin Outlet
 # - Coin Acceptor
@@ -16,5 +43,20 @@
 # - Menu
 # - Drink Outlet
 # - Waste Water Box
+
+
+# Requirement
+# - Report
+#     - Describe inventory quantity.
+# - Off
+#     - Secret word to end program.
+# - Check Resources Sufficient
+#     - Example: Sorry! There is not enough water.
+# - Process Coins
+#     - Example: Sorry! That is not enough money. Money refunded.
+# - Check Transaction Successful
+#     - Example: Here is $2.14 in change.
+# - Make Coffee
+#     - Example: Here is your espresso. Enjoy!
 
 
