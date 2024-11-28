a57bb91f0ebd9a54203cdcb1abe0a9fdb4fa5e6b
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-27 23:29:00 -0600

    Update higher_lower_game.py project.

--- 100DaysOfCodeProjects/day-14/higher_lower_game.py
+++ 100DaysOfCodeProjects/day-14/higher_lower_game.py
@@ -2,7 +2,7 @@ import random
 import game_data
 
 
-print("HIGHER or LOWER")
+print("\nHIGHER or LOWER\n")
 
 
 score = 0
@@ -13,7 +13,7 @@ end_game = False
 
 while end_game != True:
     if score > 0:
-        print(f"You are right! Current score: {score}.")
+        print(f"\nYou are right! Current score: {score}.\n")
 
 
     # Could get the same Instagram user.
@@ -22,14 +22,14 @@ while end_game != True:
     instagram_b = random.choice((game_data.data))
 
 
-    print(f"Compare A: {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.")
-    print(f"Follower Count: {instagram_a['follower_count']}.")
-    print(f"Compare B: {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.")
-    print(f"Follower Count: {instagram_b['follower_count']}.")
+    print(f"\nCompare A: {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.")
+    #print(f"Follower Count: {instagram_a['follower_count']}.\n")
+    print(f"\nCompare B: {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.")
+    #print(f"Follower Count: {instagram_b['follower_count']}.\n")
 
 
     # TO-DO: Make this a separate function.
-    player_choice = input("Who has more followers? Type 'A' or 'B': ")
+    player_choice = input("\nWho has more followers? Type 'A' or 'B': ")
     if player_choice.lower() == "a":
         player_choice = instagram_a
     elif player_choice.lower() == "b":
@@ -40,18 +40,18 @@ while end_game != True:
 
     # TO-DO: Refactor. Separate function?
     if instagram_a['follower_count'] > instagram_b['follower_count']:
-        print(f"More Followers ({instagram_a['follower_count']}): {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.")
+        print(f"\nMore Followers ({instagram_a['follower_count']}): {instagram_a['name']}, a {instagram_a['description']}, from {instagram_a['country']}.\n")
         if player_choice == instagram_a:
             score += 1
         else:
-            print(f"Sorry! That is wrong. Final score: {score}.")
+            print(f"\nSorry! That is wrong. Final score: {score}.\n")
             end_game = True
     else:
-        print(f"More Followers ({instagram_b['follower_count']}): {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.")
+        print(f"\nMore Followers ({instagram_b['follower_count']}): {instagram_b['name']}, a {instagram_b['description']}, from {instagram_b['country']}.\n")
         if player_choice == instagram_b:
             score += 1
         else:
-            print(f"Sorry! That is wrong. Final score: {score}.")
+            print(f"\nSorry! That is wrong. Final score: {score}.\n")
             end_game = True
 
 
