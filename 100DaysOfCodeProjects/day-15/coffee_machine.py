8bb3b0afadfce1a13ed3ab57348c7f317d7dbcac
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-28 15:59:08 -0600

    Update coffee_machine.py project.

--- 100DaysOfCodeProjects/day-15/coffee_machine.py
+++ 100DaysOfCodeProjects/day-15/coffee_machine.py
@@ -1,3 +1,93 @@
+import menu
+
+
+resource = {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+        "money": 0,
+}
+
+
+def menu_choice(user_coffee_input):
+    match user_coffee_input.lower():
+        case "espresso":
+            return menu.MENU['espresso']
+        case "latte":
+            return menu.MENU['latte']
+        case "cappuccino":
+            return menu.MENU['cappuccino']
+        case "report":
+            print(f"Water: {resource['water']} mL")
+            print(f"Milk: {resource['milk']} mL")
+            print(f"Coffee: {resource['coffee']} g")
+            print(f"Money: ${resource['money']:.2f}")
+            return 1
+        case "off":
+            print(f"Shutting down...")
+            return 0
+        case _:
+            print("Invalid choice. Please try again.")
+            return 1
+
+
+def total_money(quarters, dimes, nickels, pennies):
+    total = 0
+    total += quarters * 0.25
+    total += dimes * 0.10
+    total += nickels * 0.05
+    total += pennies * 0.01
+    return total
+
+
+# Change from True / False to numerical solution to do match case.
+# Match case allows to be specific for the resource that does not have enough.
+def resources_check(machine_resource, machine_coffee):
+    if machine_resource['money'] < machine_coffee['cost']:
+        return False
+    else:
+        for ingredient in machine_coffee['ingredients']:
+            #print(coffee['ingredients'][ingredient])
+            if machine_coffee['ingredients'][ingredient] > machine_resource[ingredient]:
+                return False
+        return True
+
+
+def resources_sum(machine_resource, machine_coffee):
+    resource = machine_resource
+    resource['money'] = machine_resource['money'] - machine_coffee['cost']
+    for ingredient in machine_coffee['ingredients']:
+        resource[ingredient] = machine_resource[ingredient] - machine_coffee['ingredients'][ingredient]
+    return resource
+
+
+choice_coffee = input("What would you like? (Espresso / Latte / Cappuccino): ")
+coffee = menu_choice(choice_coffee)
+
+
+choice_quarters = int(input("How many quarters? "))
+choice_dimes = int(input("How many dimes? "))
+choice_nickels = int(input("How many nickels? "))
+choice_pennies = int(input("How many pennies? "))
+
+
+resource["money"] = total_money(choice_quarters, choice_dimes, choice_nickels, choice_pennies)
+
+
+print(resources_check(resource, coffee))
+
+
+print(f"${resource['money']:.2f}")
+
+
+print(resource)
+
+resource = resources_sum(resource, coffee)
+
+print(resource)
+
+
+print(f"${resource['money']:.2f}")
 
 
 # TO-DO
