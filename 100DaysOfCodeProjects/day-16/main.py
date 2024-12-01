5b3751bc8df3ecf85630a0bd201d9e4213af2c04
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-01 12:21:12 -0600

    Update main.py file.

--- 100DaysOfCodeProjects/day-16/main.py
+++ 100DaysOfCodeProjects/day-16/main.py
@@ -3,6 +3,26 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
+menu = Menu()
+coffeemaker = CoffeeMaker()
+
+
+coffeemaker.report()
+
+
+choice_coffee = input("What would you like? Espresso / Latte / Cappuccino: ")
+
+
+drink = menu.find_drink(choice_coffee)
+
+
+if drink == None:
+    drink
+else:
+    if coffeemaker.is_resource_sufficient(drink) == True:
+        coffeemaker.make_coffee(drink)
+
+
 # TO-DO
 # 1. Print Report
 # 2. Check Resources Sufficient
