492bc5f4409244d3a8b9245f7c3ab047db26a4ab
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-30 20:28:40 -0600

    Begin OOP coffee machine project.

---
+++ 100DaysOfCodeProjects/day-16/main.py
@@ -0,0 +1,13 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+# TO-DO
+# 1. Print Report
+# 2. Check Resources Sufficient
+# 3. Process Coins
+# 4. Make Transaction Successful
+# 5. Make Coffee
+
+
