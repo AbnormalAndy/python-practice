4b355eef97b16322af0eefbe760cb9ffb78e34e2
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-02 19:14:08 -0600

    Update methods and attributes in quiz_brain.py as well as code in main.py file.

--- 100DaysOfCodeProjects/day-17/main.py
+++ 100DaysOfCodeProjects/day-17/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import Brain
 
 
 question_bank = []
@@ -10,6 +11,18 @@ for i in question_data:
     question_bank.append(i)
 
 
-print(question_bank)
+brain = Brain(question_bank)
+
+
+end_game = False
+
+
+while end_game != True:
+    brain.next_question()
+    end_game = brain.still_has_questions()
+
+
+print("You have completed the quiz!")
+print(f"Final Score: {brain.score}/{brain.question_number}")
 
 
