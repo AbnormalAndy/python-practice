f293f17ed6a77c213604de5444aab38572aad00d
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-02 18:19:41 -0600

    Add data, Question class, and create question_bank.

--- 100DaysOfCodeProjects/day-17/main.py
+++ 100DaysOfCodeProjects/day-17/main.py
@@ -1,5 +1,16 @@
-# MAIN FILE
+from data import question_data
+from question_model import Question
 
+
+question_bank = []
+
+
+for i in question_data:
+    i = Question(i['text'], i['answer'])
+    question_bank.append(i)
+
+
+print(question_bank)
 
 
 # TO-DO
--- 100DaysOfCodeProjects/day-17/question_model.py
+++ 100DaysOfCodeProjects/day-17/question_model.py
@@ -1,3 +1,7 @@
-# QUESTION MODEL FILE
+class Question:
+
+    def __init__(self, question_text, question_answer):
+        self.text = question_text
+        self.answer = question_answer
 
 
