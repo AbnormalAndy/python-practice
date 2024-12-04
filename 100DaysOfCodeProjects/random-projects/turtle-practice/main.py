becf85880e2c866cb6601b418d66a31f0c0fd7d2
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-03 20:28:21 -0600

    Add code to make turtle draw a square.

--- 100DaysOfCodeProjects/random-projects/turtle-practice/main.py
+++ 100DaysOfCodeProjects/random-projects/turtle-practice/main.py
@@ -3,10 +3,29 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape('circle')
-timmy_the_turtle.color('blue')
+timmy_the_turtle.shapesize(1, 1, 10)
+timmy_the_turtle.color('white')
+
+
+timmy_the_turtle.pencolor('black')
+timmy_the_turtle.forward(180)
+
+
+timmy_the_turtle.pencolor('white')
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(180)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(360)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(360)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(360)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(180)
 
 
 screen = Screen()
+screen.bgcolor('black')
 screen.exitonclick()
 
 
