ae4f3fbc294f68717fb7a01d5a130d01ed3d470c
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-03 20:10:19 -0600

    Add turtle-practice.

---
+++ 100DaysOfCodeProjects/random-projects/turtle-practice/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('circle')
+timmy_the_turtle.color('blue')
+
+
+screen = Screen()
+screen.exitonclick()
+
+
