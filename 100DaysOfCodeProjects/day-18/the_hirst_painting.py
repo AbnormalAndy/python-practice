6536fe211de29ea7abab7acd973da691ba000e79
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-08 19:49:22 -0600

    Add code for the_hirst_painting.py project.

---
+++ 100DaysOfCodeProjects/day-18/the_hirst_painting.py
@@ -0,0 +1,61 @@
+import color_palette
+import random
+import turtle as t
+
+
+turtle = t.Turtle()
+t.colormode(255)
+
+
+turtle.shape('circle')
+turtle.ht()
+turtle.shapesize(0.1, 0.1, 0)
+turtle.speed(0)
+
+
+def random_color():
+    color = random.choice(color_palette.colors)
+    return color
+
+
+def painting(painting_width, painting_height):
+    x = (painting_width / 2) * 50
+    y = (painting_height / 2) * 50
+
+
+    beginning_position = [-x, -y]
+
+
+    turtle.teleport(beginning_position[0], beginning_position[1])
+    
+
+    height = 0
+
+
+    while height <= painting_height:
+
+
+        width = 0
+
+
+        while width <= painting_width:
+            turtle.dot(10, random_color())
+            width += 1
+            beginning_position[0] += 50
+            turtle.teleport(beginning_position[0], beginning_position[1])
+
+
+
+        height += 1
+        beginning_position[0] = -x
+        beginning_position[1] += 50
+        turtle.teleport(beginning_position[0], beginning_position[1])
+    
+
+painting(13, 13)
+
+
+screen = t.Screen()
+screen.exitonclick()
+
+
