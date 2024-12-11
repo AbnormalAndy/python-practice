bf86fdf8f8e9857660bd648342bba8208700cfdf
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-10 21:34:07 -0600

    Update turtle_race.py project.

--- 100DaysOfCodeProjects/day-19/turtle_race.py
+++ 100DaysOfCodeProjects/day-19/turtle_race.py
@@ -29,9 +29,6 @@ def random_color():
     return color
 
 
-turtle_list = []
-
-
 def turtle_create():
     turtle_color = random_color()
     turtle = {}
@@ -40,8 +37,35 @@ def turtle_create():
     return turtle
 
 
+def user_guess_outcome(turtle_winner_position, user_guess):
+    if turtle_winner_position == user_guess:
+        return True
+    else:
+        return False
+
+
+screen = t.Screen()
+user_guess = screen.numinput("Turtle RACE", "Who will win the race? Enter a position (1-10):", minval=1, maxval=10)
+user_guess -= 1
+
+
+finish_line = t.Turtle()
+finish_line.ht()
+finish_line.pensize(3)
+finish_line.teleport(300, 400)
+finish_line.right(90)
+finish_line.forward(800)
+
+
 turtle_count = 10
-position_start = (turtle_count * 50) / 2
+
+
+x_position_start = -300
+y_position_start = 300
+y_position_spacing = 600 / turtle_count
+
+
+turtle_list = []
 
 
 for i in range(turtle_count):
@@ -52,39 +76,31 @@ for i in range(len(turtle_list)):
     turtle_list[i]['name'] = t.Turtle()
     turtle_list[i]['name'].shape('turtle')
     turtle_list[i]['name'].color(turtle_list[i]['color'])
-    turtle_list[i]['name'].teleport(0, position_start)
-    position_start -= 50
+    turtle_list[i]['name'].teleport(x_position_start, y_position_start)
+    turtle_list[i]['name'].penup()
+    y_position_start -= y_position_spacing
 
 
-print(turtle_list)
+end_race = False
 
 
-# This can be a for loop:
-# - Generates a turtle.
-# - Chooses a color.
-# - Teleports to starting line.
-#     - 25, 50, 50 = 125
+while end_race != True:
+    for i in range(len(turtle_list)):
+        turtle_list[i]['name'].forward(random.randint(0, 100))
+        if turtle_list[i]['name'].position()[0] > 300:
+            winner = i
+            #print(f"The {turtle_list[i]['color']} turtle at the {i + 1} position wins the race!")
+            end_race = True
+            break
 
 
-'''
-turtle_purple = t.turtle()
-turtle_blue = t.turtle()
-turtle_green = t.turtle()
-turtle_yellow = t.turtle()
-turtle_orange = t.turtle()
-turtle_red = t.turtle()
-'''
-
-
-screen = t.Screen()
 screen.exitonclick()
 
 
-# TO-DO
-# - Create six turtles.
-#    - Each turtle is a different color.
-# - Randomize the movement forward for each.
-#    - Function for this?
-# - Declare a winner by creating an arbitrary line.
-#    - Could be if a turtle makes it to such coordinate, wins.
-#    - Could be if a turtle travels a certain distance, wins.
+print(f"The {turtle_list[winner]['color']} turtle at the {winner + 1} position wins the race!")
+if user_guess_outcome(winner, user_guess):
+    print("You win!")
+else:
+    print("You lose!")
+
+
