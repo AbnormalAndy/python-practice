2e336d8f2218a8b302c2f32aa1986f03c7b4282a
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-12-15 12:51:55 -0600

    Create Snake class, add create_snake() and move().

--- 100DaysOfCodeProjects/day-20/main.py
+++ 100DaysOfCodeProjects/day-20/main.py
@@ -1,68 +1,45 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+
+
+# TURN BACK ON
 screen.tracer(0)
 
 
-segment_list = []
+snake = Snake()
 
 
-# Starting positions in tuple.
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+game_is_off = False
 
 
-# Creates new segments.
-for position in starting_positions:
-    segment = Turtle()
-    segment.shape('square')
-    segment.color('white')
-    segment.penup()
-    segment.goto(position)
-    segment_list.append(segment)
-
-
-screen.update()
-
-
-end = False
-
-
-# Segments move together.
-while end != True:
-    for segment_number in range(len(segment_list) - 1, 0, -1):
-        new_x = segment_list[segment_number - 1].xcor()
-        new_y = segment_list[segment_number - 1].ycor()
-        segment_list[segment_number].goto(new_x, new_y)
-
-
-    segment_list[0].forward(10)
-    segment_list[0].left(90)
-
-
+while game_is_off != True:
     screen.update()
+    time.sleep(0.1)
 
 
-    if segment_list[0].xcor() > 270:
-        end = True
+    snake.move()
 
 
+    if snake.segments[0].xcor() > 270 or snake.segments[0].xcor() < -270:
+        game_is_off = True
 
-# Turtle in for loop.
-# If add to loop, add another segment?
+
+    if snake.segments[0].ycor() > 270 or snake.segments[0].ycor() < -270:
+        game_is_off = True
+
 
 
 screen.exitonclick()
 
 
 # TO-DO:
-# Create snake body.
-# - 3 squares.
-# - 20x20 = Turtle
-
 # Move snake.
 # - Up, down, left, right.
 
@@ -71,3 +48,7 @@ screen.exitonclick()
 # Create Scoreboard
 # Detect Collision with Wall
 # Detect Collision with Tail
+
+# Snake Class
+# Food Class
+# Scoreboard Class
---
+++ 100DaysOfCodeProjects/day-20/snake.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            segment = Turtle('square')
+            segment.color('white')
+            segment.penup()
+            segment.goto(position)
+            self.segments.append(segment)
+
+        
+    def move(self):
+        for segment_number in range(len(self.segments) - 1, 0, -1):
+            x_new = self.segments[segment_number - 1].xcor()
+            y_new = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(x_new, y_new)
+
+        
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
