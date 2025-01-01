daa7b8f32603421c0a97d47311ef9959047aa74c
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-01 15:10:56 -0600

    Add code to move padde up and down.

--- 100DaysOfCodeProjects/day-22/main.py
+++ 100DaysOfCodeProjects/day-22/main.py
@@ -1,5 +1,6 @@
 from paddle import Paddle
 from turtle import Screen, Turtle
+import time
 
 
 WIDTH, HEIGHT = 1200, 600
@@ -12,10 +13,29 @@ screen.title('Pong')
 screen.bgcolor('black')
 
 
+screen.tracer(0)
+
+
 screen.setup(WIDTH + 4, HEIGHT + 8)
 
 
 paddle = Paddle()
+
+
+#paddle_player = paddle.create_paddle_player()
+#paddle_computer = paddle.create_paddle_computer()
+
+
+screen.listen()
+screen.onkey(paddle.move_up, "Up")
+screen.onkey(paddle.move_down, "Down")
+
+
+game_is_off = False
+
+
+while game_is_off != True:
+    screen.update()
 
 
 screen.exitonclick()
--- 100DaysOfCodeProjects/day-22/paddle.py
+++ 100DaysOfCodeProjects/day-22/paddle.py
@@ -4,20 +4,29 @@ from turtle import Turtle
 COLOR = 'white'
 SHAPE = 'square'
 MOVE_DISTANCE = 20
-STARTING_POSITION = [(550, 20), (550, 0), (550, -20)]
+STARTING_POSITION_PLAYER = [(550, 20), (550, 0), (550, -20)]
+#STARTING_POSITION_COMPUTER = [(-550, 20), (-550, 0), (-550, -20)]
+
+
+UP = 90
+DOWN = 270
 
 
 class Paddle:
     def __init__(self):
         self.segments = []
-        self.create_paddle()
+        self.create_paddle_player()
+        self.paddle_top = self.segments[0]
+        self.paddle_bottom = self.segments[2]
+        self.paddle_top.setheading(UP)
+        self.paddle_bottom.setheading(DOWN)
 
 
-    def create_paddle(self):
-        for position in STARTING_POSITION:
+    def create_paddle_player(self):
+        for position in STARTING_POSITION_PLAYER:
             self.add_segment(position)
 
-        
+
     def add_segment(self, position):
         segment = Turtle()
         segment.penup()
@@ -27,9 +36,34 @@ class Paddle:
         self.segments.append(segment)
 
 
+    def move_up(self):
+        for segment_number in range(len(self.segments) - 1, 0, -1):
+            x_new = self.segments[segment_number - 1].xcor()
+            y_new = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(x_new, y_new)
+
+
+        self.paddle_top.forward(MOVE_DISTANCE)
+
+
+    def move_down(self):
+        for segment_number in range(len(self.segments) - 1):
+            x_new = self.segments[segment_number + 1].xcor()
+            y_new = self.segments[segment_number + 1].ycor()
+            self.segments[segment_number].goto(x_new, y_new)
+
+
+        self.paddle_bottom.forward(MOVE_DISTANCE)
+
+
 # TO-DO
 # Move up and down.
 # - Need to iterate through the segments.
 #     - Move each segment by MOVE_DISTANCE.
 #     - Move up by leading with first position?
 #     - Move down by leading with last position?
+
+# - Set border limits.
+#     - Main or Paddle file?
+
+
