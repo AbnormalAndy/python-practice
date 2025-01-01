910514ffd13291eeede54142e30da7b8d7491f46
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-01 15:46:17 -0600

    Add constant for player difficulty.

--- 100DaysOfCodeProjects/day-22/main.py
+++ 100DaysOfCodeProjects/day-22/main.py
@@ -6,7 +6,10 @@ import time
 WIDTH, HEIGHT = 1200, 600
 
 
-STARTING_POSITION_PLAYER = [(550, 20), (550, 0), (550, -20)]
+STARTING_POSITION_PLAYER_EASY = [(550, 40), (550, 20), (550, 0), (550, -20), (550, -40)]
+STARTING_POSITION_PLAYER_HARD = [(550, 20), (550, 0), (550, -20)]
+
+
 STARTING_POSITION_COMPUTER_EASY = [(-550, 20), (-550, 0), (-550, -20)]
 STARTING_POSITION_COMPUTER_HARD = [(-550, 40), (-550, 20), (-550, 0), (-550, -20), (-550, -40)]
 
@@ -24,7 +27,7 @@ screen.tracer(0)
 screen.setup(WIDTH + 4, HEIGHT + 8)
 
 
-paddle_player = Paddle(STARTING_POSITION_PLAYER)
+paddle_player = Paddle(STARTING_POSITION_PLAYER_EASY)
 paddle_computer = Paddle(STARTING_POSITION_COMPUTER_EASY)
 
 
