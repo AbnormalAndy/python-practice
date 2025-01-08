3bb0469ff50741dd3b82cd00f4124052fcd8f480
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-08 13:08:50 -0600

    Add code to move computer paddle.

--- 100DaysOfCodeProjects/day-22/main.py
+++ 100DaysOfCodeProjects/day-22/main.py
@@ -18,6 +18,12 @@ STARTING_POSITION_COMPUTER_EASY = [(-550, 20), (-550, 0), (-550, -20)]
 STARTING_POSITION_COMPUTER_HARD = [(-550, 40), (-550, 20), (-550, 0), (-550, -20), (-550, -40)]
 
 
+# Difficulty
+EASY = 8
+MEDIUM = 6
+HARD = 4
+
+
 screen = Screen()
 
 
@@ -32,7 +38,7 @@ screen.setup(WIDTH + 4, HEIGHT + 8)
 
 
 paddle_player = Paddle(STARTING_POSITION_PLAYER_EASY)
-paddle_computer = Paddle(STARTING_POSITION_COMPUTER_EASY)
+paddle_computer = Paddle(STARTING_POSITION_COMPUTER_HARD)
 
 
 ball = Ball()
@@ -50,6 +56,7 @@ screen.onkey(paddle_player.move_down, "Down")
 
 
 game_is_off = False
+counter = 0
 
 
 while game_is_off != True:
@@ -88,6 +95,24 @@ while game_is_off != True:
         ball.setheading(heading)
 
 
+    print(ball.ycor())
+    print(paddle_computer.segments[1].ycor())
+
+
+    # Moves computer paddle. Counter delays computer input to not automatically win.
+    if counter == EASY:
+        if paddle_computer.segments[1].ycor() >= 0 and paddle_computer.segments[1].ycor() < ball.ycor():
+            paddle_computer.move_up()
+        if paddle_computer.segments[1].ycor() >= 0 and paddle_computer.segments[1].ycor() > ball.ycor():
+            paddle_computer.move_down()
+        if paddle_computer.segments[1].ycor() <= 0 and paddle_computer.segments[1].ycor() < ball.ycor():
+            paddle_computer.move_up()
+        if paddle_computer.segments[1].ycor() <= 0 and paddle_computer.segments[1].ycor() > ball.ycor():
+            paddle_computer.move_down()
+
+        counter = 0
+
+
     # Score for player paddle.
     if ball.xcor() < -600:
         scoreboard.increase_score_player()
@@ -114,6 +139,11 @@ while game_is_off != True:
         scoreboard.game_over(scoreboard.computer)
 
 
+    counter += 1
+
+
 screen.exitonclick()
 
 
+# TO-DO
+# - Deflecting ball off paddle is a full 180 when it should be 90?
