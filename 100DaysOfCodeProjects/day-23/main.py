5d3dca918238f1c2ae45738de7e15fc26a5b9901
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-09 19:04:07 -0600

    Setup screen in main.py file.

---
+++ 100DaysOfCodeProjects/day-23/main.py
@@ -0,0 +1,17 @@
+from turtle import Screen
+
+
+WIDTH, HEIGHT = 600, 600
+
+
+screen = Screen()
+
+
+screen.title('Turtle Crossing')
+screen.bgcolor('black')
+
+
+screen.setup(WIDTH + 4, HEIGHT + 8)
+
+
+screen.exitonclick()
