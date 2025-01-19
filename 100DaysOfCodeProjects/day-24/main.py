be051c3905bc6714da09e0722bbd451d4341d4b1
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-18 21:04:59 -0600

    Add code to add a variety of names to a letter and write those letters to a new directory.

--- 100DaysOfCodeProjects/day-24/main.py
+++ 100DaysOfCodeProjects/day-24/main.py
@@ -1,14 +1,25 @@
 with open('./Input/Letters/starting_letter.txt', 'r') as file:
     letter = file.readlines()
 
-print(letter)
-letter[0] = letter[0].replace('[name]', 'meow')
-print(letter)
-
 
 with open('./Input/Names/invited_names.txt', 'r') as file:
     names = file.readlines()
-    for name in names:
-        print(name)
+
+
+for name in names:
+    new_name = name.strip('\n')
+    new_letter = [l.replace('[name]', new_name) for l in letter]
+
+
+    with open(f'./Output/ReadyToSend/invite_for_{new_name}.txt', mode='w') as file:
+        file.writelines(new_letter)
+
+
+# Why does this work:
+# - new_letter = [l.replace('[name]', new_name) for l in letter]
+
+# Why does this NOT work:
+# - new_letter[0] = letter[0].replace('[name]', new_name)
+#     - This seems to replace the original list.
 
 
