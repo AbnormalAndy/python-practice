025e31ff3214a7c4323ee1ac43ce2864d6c2c776
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-21 16:14:32 -0600

    Add pandas practice code.

--- 100DaysOfCodeProjects/day-25/practice/pandas_main.py
+++ 100DaysOfCodeProjects/day-25/practice/pandas_main.py
@@ -10,3 +10,36 @@ print(data['temp'])
 print(data['condition'])
 
 
+temp_average = data['temp'].mean()
+print(f"{temp_average:.2f}")
+
+
+temp_max = data['temp'].max()
+print(temp_max)
+
+
+print(data[data.temp == data.temp.max()])
+
+
+temp_min = data['temp'].min()
+print(temp_min)
+
+
+print(data[data.temp == data.temp.min()])
+
+
+monday = data[data.day == 'Monday']
+print(monday.temp)
+
+
+data_dict = {
+        "student": ["Amy", "James", "Angela"],
+        "scores": [76, 56, 65],
+}
+
+
+data = pandas.DataFrame(data_dict)
+print(data)
+#data.to_csv("student_data.csv")
+
+
