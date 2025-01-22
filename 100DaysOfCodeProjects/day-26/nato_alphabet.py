23014fc923a58fc43efd636160bcdfc48c8b61b0
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-22 14:05:10 -0600

    Add code for nato_alphabet.py file.

---
+++ 100DaysOfCodeProjects/day-26/nato_alphabet.py
@@ -0,0 +1,14 @@
+import pandas
+
+
+nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
+nato_alphabet_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
+
+
+user_input = input('\nEnter a word: ').upper()
+user_input_nato_alphabet = [nato_alphabet_dict[letter] for letter in user_input if letter in nato_alphabet_dict]
+
+
+print(f'\nNato Alphabet Output: {"-".join(user_input_nato_alphabet)}\n')
+
+
