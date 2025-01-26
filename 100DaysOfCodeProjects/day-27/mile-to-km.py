b85ed7cd321af1a7befe0adc2521f0a4b4e6f886
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-01-26 17:25:40 -0600

    Add code for the window of the mile-to-km.py project.

---
+++ 100DaysOfCodeProjects/day-27/mile-to-km.py
@@ -0,0 +1,22 @@
+import tkinter
+
+
+window = tkinter.Tk()
+window.title('Mile to Km Converter')
+window.minsize(width=400, height=200)
+window.config(padx=20, pady=20)
+
+
+
+
+
+window.mainloop()
+
+
+# TO-DO
+# - Title: Mile to Km Converter
+# - Input Miles
+# - is equal to <number> Km
+# - Button to Calculate
+
+
--- 100DaysOfCodeProjects/day-27/practice/practice_tkinter.py
+++ 100DaysOfCodeProjects/day-27/practice/practice_tkinter.py
@@ -1,26 +1,30 @@
 import tkinter
 
 
+def button_clicked():
+    #my_lable.config(text='I clicked the button!')
+    my_label.config(text=entry.get())
+
+
 window = tkinter.Tk()
 window.title('My First GUI Program')
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 
-def button_clicked():
-    #my_lable.config(text='I clicked the button!')
-    my_label.config(text=input.get())
-
-
+# Label
 my_label = tkinter.Label(text='I am a Label', font=('Futura', 24, 'bold'))
 my_label.pack()
 
 
+# Button
 button = tkinter.Button(text='Click Me', command=button_clicked)
 button.pack()
 
 
-input = tkinter.Entry(width=10)
-input.pack()
+# Entry
+entry = tkinter.Entry(width=10)
+entry.pack()
 
 
 window.mainloop()
