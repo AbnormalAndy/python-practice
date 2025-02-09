112f67d56b1b79291c1ef516d4e281bd63d1d000
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-09 00:00:27 -0600

    Increase time for timer.

--- 100DaysOfCodeProjects/day-28/main.py
+++ 100DaysOfCodeProjects/day-28/main.py
@@ -46,9 +46,9 @@ def start_timer():
     reps += 1
 
 
-    work_sec = WORK_MIN #* 60
-    short_break_sec = SHORT_BREAK_MIN #* 60
-    long_break_sec = LONG_BREAK_MIN #* 60
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
 
     # Rep 8/16/24 - Long Break
