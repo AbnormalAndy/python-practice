44dde0757ae56db4f48e471877746faa70e347df
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-10 17:20:54 -0600

    Start password manager project.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -11,11 +11,13 @@ from tkinter import *
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Window Title
 window = Tk()
 window.title('Password Manager')
 window.config(padx=20, pady=20)
 
 
+# Imports Image
 logo_img = PhotoImage(file='logo.png')
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_img)
