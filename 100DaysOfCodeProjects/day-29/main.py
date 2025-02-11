77640c646ef678b4c175e212feb695eec33bf6b9
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-11 17:37:23 -0600

    Start password manager project.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -21,7 +21,7 @@ window.config(padx=20, pady=20)
 logo_img = PhotoImage(file='logo.png')
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=1, column=2)
 
 
 window.mainloop()
