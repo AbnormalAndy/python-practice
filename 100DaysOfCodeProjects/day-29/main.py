fdae84bef459501e6eabcc76ebd3aebdb8996258
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-12 17:54:07 -0600

    Add code for UI.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -24,6 +24,38 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=1, column=2)
 
 
+website_label = Label(text='Website', font=('Futura'), anchor='e')
+website_label.grid(sticky = E, row=2, column=1)
+
+
+website_entry = Entry(width=35)
+website_entry.grid(row=2, column=2, columnspan=2)
+
+
+email_label = Label(text='Email/Username', font=('Futura'), anchor='e')
+email_label.grid(sticky = E, row=3, column=1)
+
+
+email_entry = Entry(width=35)
+email_entry.grid(row=3, column=2, columnspan=2)
+
+
+password_label = Label(text='Password', font=('Futura'), anchor='e')
+password_label.grid(sticky = E, row=4, column=1)
+
+
+password_entry = Entry(width=21)
+password_entry.grid(row=4, column=2)
+
+
+generate_password_button = Button(text='Generate Password', font=('Futura'))
+generate_password_button.grid(row=4, column=3)
+
+
+add_button = Button(text='Add', font=('Futura'), width=36)
+add_button.grid(row=5, column=2, columnspan=2)
+
+
 window.mainloop()
 
 
