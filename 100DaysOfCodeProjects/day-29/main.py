39ad74ffb9b5254d95f6a2fa6c38ac8eff6b3b56
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-12 17:58:05 -0600

    Add constants.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 
 
+# ---------------------------- CONSTANTS ------------------------------- #
+
+FONT_NAME = 'Futura'
+RIGHT_JUSTIFY = 'e'
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -24,7 +30,7 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=1, column=2)
 
 
-website_label = Label(text='Website', font=('Futura'), anchor='e')
+website_label = Label(text='Website', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 website_label.grid(sticky = E, row=2, column=1)
 
 
@@ -32,7 +38,7 @@ website_entry = Entry(width=35)
 website_entry.grid(row=2, column=2, columnspan=2)
 
 
-email_label = Label(text='Email/Username', font=('Futura'), anchor='e')
+email_label = Label(text='Email/Username', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 email_label.grid(sticky = E, row=3, column=1)
 
 
@@ -40,7 +46,7 @@ email_entry = Entry(width=35)
 email_entry.grid(row=3, column=2, columnspan=2)
 
 
-password_label = Label(text='Password', font=('Futura'), anchor='e')
+password_label = Label(text='Password', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 password_label.grid(sticky = E, row=4, column=1)
 
 
@@ -48,11 +54,11 @@ password_entry = Entry(width=21)
 password_entry.grid(row=4, column=2)
 
 
-generate_password_button = Button(text='Generate Password', font=('Futura'))
+generate_password_button = Button(text='Generate Password', font=(FONT_NAME))
 generate_password_button.grid(row=4, column=3)
 
 
-add_button = Button(text='Add', font=('Futura'), width=36)
+add_button = Button(text='Add', font=(FONT_NAME), width=36)
 add_button.grid(row=5, column=2, columnspan=2)
 
 
