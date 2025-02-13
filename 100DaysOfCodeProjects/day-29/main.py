b1ae12f8ef5f6fbe572fa1ee4ee1387b8705f6ca
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-13 17:34:22 -0600

    Add comments.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -30,34 +30,42 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=1, column=2)
 
 
+# Website Label
 website_label = Label(text='Website', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 website_label.grid(sticky = E, row=2, column=1)
 
 
+# Website Text Box
 website_entry = Entry(width=35)
 website_entry.grid(row=2, column=2, columnspan=2)
 
 
+# Email Label
 email_label = Label(text='Email/Username', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 email_label.grid(sticky = E, row=3, column=1)
 
 
+# Email Text Box
 email_entry = Entry(width=35)
 email_entry.grid(row=3, column=2, columnspan=2)
 
 
+# Password Label
 password_label = Label(text='Password', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
 password_label.grid(sticky = E, row=4, column=1)
 
 
+# Password Text Box
 password_entry = Entry(width=21)
 password_entry.grid(row=4, column=2)
 
 
+# Generate Password Button
 generate_password_button = Button(text='Generate Password', font=(FONT_NAME))
 generate_password_button.grid(row=4, column=3)
 
 
+# Add Button
 add_button = Button(text='Add', font=(FONT_NAME), width=36)
 add_button.grid(row=5, column=2, columnspan=2)
 
