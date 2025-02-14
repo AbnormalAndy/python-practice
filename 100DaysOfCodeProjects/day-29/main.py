c4a384f1edcb3c47c9aad7864da893012dd9bfc8
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-14 17:59:19 -0600

    Change row and column numbers.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -27,47 +27,47 @@ window.config(padx=20, pady=20)
 logo_img = PhotoImage(file='logo.png')
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=1, column=2)
+canvas.grid(row=0, column=1)
 
 
 # Website Label
 website_label = Label(text='Website', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-website_label.grid(sticky = E, row=2, column=1)
+website_label.grid(sticky = E, row=1, column=0)
 
 
 # Website Text Box
 website_entry = Entry(width=35)
-website_entry.grid(row=2, column=2, columnspan=2)
+website_entry.grid(row=1, column=1, columnspan=2)
 
 
 # Email Label
 email_label = Label(text='Email/Username', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-email_label.grid(sticky = E, row=3, column=1)
+email_label.grid(sticky = E, row=2, column=0)
 
 
 # Email Text Box
 email_entry = Entry(width=35)
-email_entry.grid(row=3, column=2, columnspan=2)
+email_entry.grid(row=2, column=1, columnspan=2)
 
 
 # Password Label
 password_label = Label(text='Password', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-password_label.grid(sticky = E, row=4, column=1)
+password_label.grid(sticky = E, row=3, column=0)
 
 
 # Password Text Box
 password_entry = Entry(width=21)
-password_entry.grid(row=4, column=2)
+password_entry.grid(row=3, column=1)
 
 
 # Generate Password Button
 generate_password_button = Button(text='Generate Password', font=(FONT_NAME))
-generate_password_button.grid(row=4, column=3)
+generate_password_button.grid(row=3, column=2)
 
 
 # Add Button
 add_button = Button(text='Add', font=(FONT_NAME), width=36)
-add_button.grid(row=5, column=2, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
