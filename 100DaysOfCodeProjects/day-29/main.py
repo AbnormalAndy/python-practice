7f55dbbe3b03a144bc5c0c74e97e9a3616471e83
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-21 12:23:25 -0600

    Change code.

--- 100DaysOfCodeProjects/day-29/main.py
+++ 100DaysOfCodeProjects/day-29/main.py
@@ -1,54 +1,52 @@
-from tkinter import *
+from tkinter import Tk, PhotoImage, Canvas, Entry, Button, Label, E
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
 
-FONT_NAME = 'Futura'
-RIGHT_JUSTIFY = 'e'
+FONT_NAME = "Futura"
+RIGHT_JUSTIFY = "e"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def add_information():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    information = f'{website} | {email} | {password}\n'
-    
+    information = f"{website} | {email} | {password}\n"
 
     # Writes information to file.
-    with open(f'data.txt', mode='a') as file:
+    with open(f"data.txt", mode="a") as file:
         file.write(information)
 
-
     # Clears entry fields of text.
-    website_entry.delete(0, 'end')
-    email_entry.delete(0, 'end')
-    password_entry.delete(0, 'end')
+    website_entry.delete(0, "end")
+    email_entry.delete(0, "end")
+    password_entry.delete(0, "end")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Window Title
 window = Tk()
-window.title('Password Manager')
+window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 
 # Imports Image
-logo_img = PhotoImage(file='logo.png')
+logo_img = PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 
 # Website Label
-website_label = Label(text='Website', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-website_label.grid(sticky = E, row=1, column=0)
+website_label = Label(text="Website", font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
+website_label.grid(sticky=E, row=1, column=0)
 
 
 # Website Text Box
@@ -58,8 +56,8 @@ website_entry.focus()
 
 
 # Email Label
-email_label = Label(text='Email/Username', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-email_label.grid(sticky = E, row=2, column=0)
+email_label = Label(text="Email/Username", font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
+email_label.grid(sticky=E, row=2, column=0)
 
 
 # Email Text Box
@@ -68,8 +66,8 @@ email_entry.grid(row=2, column=1, columnspan=2)
 
 
 # Password Label
-password_label = Label(text='Password', font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
-password_label.grid(sticky = E, row=3, column=0)
+password_label = Label(text="Password", font=(FONT_NAME), anchor=RIGHT_JUSTIFY)
+password_label.grid(sticky=E, row=3, column=0)
 
 
 # Password Text Box
@@ -78,15 +76,13 @@ password_entry.grid(row=3, column=1)
 
 
 # Generate Password Button
-generate_password_button = Button(text='Generate Password', font=(FONT_NAME))
+generate_password_button = Button(text="Generate Password", font=(FONT_NAME))
 generate_password_button.grid(row=3, column=2)
 
 
 # Add Button
-add_button = Button(text='Add', font=(FONT_NAME), width=36, command=add_information)
+add_button = Button(text="Add", font=(FONT_NAME), width=36, command=add_information)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
-
-
