d6a71de18e9a3a31665568041dd7da2421ecae22
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-24 20:19:52 -0600

    Practice with exceptions.

--- 100DaysOfCodeProjects/day-30/main.py
+++ 100DaysOfCodeProjects/day-30/main.py
@@ -2,7 +2,19 @@
 try:
     file = open('a_file.txt')
 
-except:
-    print("There was an error.")
+except FileNotFoundError:
+    file = open('a_file.txt', 'w')
+    file.write('Wrote something.')
+
+except KeyError as error_message:
+    print(f'The key {error_message} does not exist.')
+
+else:
+    content = file.read()
+    print(content)
+
+finally:
+    file.close()
+    print('File was closed.')
 
 
