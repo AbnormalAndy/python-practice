d1b3e2d6aa4490b0e6259bd6fa7517741c80dbfe
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-02-27 19:42:50 -0600

    Add error handling to NATO alphabet project.

--- 100DaysOfCodeProjects/day-26/nato_alphabet.py
+++ 100DaysOfCodeProjects/day-26/nato_alphabet.py
@@ -5,10 +5,21 @@ nato_alphabet = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_alphabet_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
 
 
-user_input = input('\nEnter a word: ').upper()
-user_input_nato_alphabet = [nato_alphabet_dict[letter] for letter in user_input]
+def generate_phonetic():
+    user_input = input('\nEnter a word: ').upper()
+    try:
+        user_input_nato_alphabet = [nato_alphabet_dict[letter] for letter in user_input]
 
 
-print(f'\nNato Alphabet Output: {"-".join(user_input_nato_alphabet)}\n')
+    except KeyError as error_message:
+        print('Please use a letter in the alphabet.')
+        generate_phonetic()
+
+
+    else:
+        print(f'\nNato Alphabet Output: {"-".join(user_input_nato_alphabet)}\n')
+
+
+generate_phonetic()
 
 
