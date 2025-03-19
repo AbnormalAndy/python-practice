5418646366e52d0a3d20fe671f8a500c5f2fe422
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-03-19 17:27:19 -0500

    Add code to post to pixela.

--- 100DaysOfCodeProjects/day-37/main.py
+++ 100DaysOfCodeProjects/day-37/main.py
@@ -8,6 +8,7 @@ PIXELA_USERNAME = config.PIXELA_USERNAME
 
 pixela_endpoint = 'https://pixe.la/v1/users'
 pixela_graph_endpoint = f'{pixela_endpoint}/{PIXELA_USERNAME}/graphs' 
+pixela_graph_post_endpoint = f'{pixela_graph_endpoint}/graphone'
 
 
 pixela_create_user_params = {
@@ -27,17 +28,22 @@ pixela_graph_config_params = {
 }
 
 
+pixela_graph_post_params = {
+    'date': '20250319',
+    'quantity': '20',
+}
+
+
 headers = {
     'X-USER-TOKEN': PIXELA_TOKEN,
 }
 
 
 #pixela_create_user_response = requests.post(url=pixela_endpoint, json=pixela_create_user_params)
+#pixela_create_graph_response = requests.post(url=pixela_graph_endpoint, json=pixela_graph_config_params, headers=headers)
+pixela_post_to_graph_response = requests.post(url=pixela_graph_post_endpoint, json=pixela_graph_post_params, headers=headers) 
 
 
-pixela_create_graph_response = requests.post(url=pixela_graph_endpoint, json=pixela_graph_config_params, headers=headers)
-
-
-print(pixela_create_graph_response.text)
+print(pixela_post_to_graph_response.text)
 
 
