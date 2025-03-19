19f877fba1e15305d73ec99a3287967669e1268e
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-03-18 19:40:27 -0500

    Add code to create graph.

--- 100DaysOfCodeProjects/day-37/main.py
+++ 100DaysOfCodeProjects/day-37/main.py
@@ -7,9 +7,10 @@ PIXELA_USERNAME = config.PIXELA_USERNAME
 
 
 pixela_endpoint = 'https://pixe.la/v1/users'
+pixela_graph_endpoint = f'{pixela_endpoint}/{PIXELA_USERNAME}/graphs' 
 
 
-pixela_user_params = {
+pixela_create_user_params = {
     'token': PIXELA_TOKEN,
     'username': PIXELA_USERNAME,
     'agreeTermsOfService': 'yes',
@@ -17,6 +18,26 @@ pixela_user_params = {
 }
 
 
-#pixela_create_user_response = requests.post(url=pixela_endpoint, json=pixela_user_params)
+pixela_graph_config_params = {
+    'id': 'graphone',
+    'name': 'Drawing Graph',
+    'unit': 'minutes',
+    'type': 'int',
+    'color': 'sora',
+}
+
+
+headers = {
+    'X-USER-TOKEN': PIXELA_TOKEN,
+}
+
+
+#pixela_create_user_response = requests.post(url=pixela_endpoint, json=pixela_create_user_params)
+
+
+pixela_create_graph_response = requests.post(url=pixela_graph_endpoint, json=pixela_graph_config_params, headers=headers)
+
+
+print(pixela_create_graph_response.text)
 
 
