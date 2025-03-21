61e54705e14a71d6dab585641e0449664906a145
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-03-21 16:54:18 -0500

    Add code for put and delete requests.

--- 100DaysOfCodeProjects/day-37/main.py
+++ 100DaysOfCodeProjects/day-37/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 import config
 import requests
 
@@ -6,10 +7,14 @@ PIXELA_TOKEN = config.PIXELA_TOKEN
 PIXELA_USERNAME = config.PIXELA_USERNAME
 
 
+today = date.today()
+
+
 # Pixela Endpoints
 pixela_endpoint = 'https://pixe.la/v1/users'
 pixela_graph_endpoint = f'{pixela_endpoint}/{PIXELA_USERNAME}/graphs' 
 pixela_graph_post_endpoint = f'{pixela_graph_endpoint}/graphone'
+pixela_graph_put_endpoint = f'{pixela_graph_endpoint}/graphone/{today.strftime("%Y%m%d")}'
 
 
 # Pixela Create User Parameters
@@ -31,11 +36,19 @@ pixela_graph_config_params = {
 }
 
 
-# Pixela Graph Update Parameters
+# Pixela Graph Post Parameters
 pixela_graph_post_params = {
-    'date': '20250319',
+    'date': today.strftime('%Y%m%d'),
     'quantity': '20',
 }
+
+
+# Pixela Graph Update Parameters
+pixela_graph_put_params = {
+    'quantity': '0',
+}
+
+
 
 
 headers = {
@@ -45,9 +58,11 @@ headers = {
 
 #pixela_create_user_response = requests.post(url=pixela_endpoint, json=pixela_create_user_params)
 #pixela_create_graph_response = requests.post(url=pixela_graph_endpoint, json=pixela_graph_config_params, headers=headers)
-pixela_post_to_graph_response = requests.post(url=pixela_graph_post_endpoint, json=pixela_graph_post_params, headers=headers) 
+#pixela_post_to_graph_response = requests.post(url=pixela_graph_post_endpoint, json=pixela_graph_post_params, headers=headers) 
+#pixela_put_to_graph_response = requests.put(url=pixela_graph_put_endpoint, json=pixela_graph_put_params, headers=headers) 
+pixela_delete_to_graph_response = requests.delete(url=pixela_graph_put_endpoint, headers=headers) 
 
 
-print(pixela_post_to_graph_response.text)
+print(pixela_delete_to_graph_response.text)
 
 
