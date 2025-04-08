22ada20db1f4f1baf9aef397f2f2c1ce6e7627b6
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-04-08 17:21:52 -0500

    Add comments.

--- 100DaysOfCodeProjects/day-38/main.py
+++ 100DaysOfCodeProjects/day-38/main.py
@@ -18,6 +18,7 @@ SHEETY_ENDPOINT = 'https://api.sheety.co/99857e1bc2a42bffc6f6a39865ac096e/myWork
 today = datetime.now()
 
 
+# Today's date and time variables.
 today_date = today.strftime('%m/%d/%Y')
 today_time = today.strftime('%H:%M:%S')
 
@@ -44,16 +45,19 @@ exercise_header = {
 }
 
 
+# Saves JSON response of exercises done and entered into Nutritionix.
 nutritionix_post_response = requests.post(url=EXERCISE_ENDPOINT, json=exercise_params, headers=exercise_header)
 nutritionix_post_response.raise_for_status()
 nutritionix_response = nutritionix_post_response.json()
 
 
+# Iterates through exercises done for the day and adds to Google Sheets.
 for i in range(len(nutritionix_response['exercises'])):
+    # Sheety JSON parameters.
     sheety_params = {
         'workout': {
-            'date': f'{today_date}',
-            'time': f'{today_time}',
+            'date': today_date,
+            'time': today_time,
             'exercise': f'{nutritionix_response['exercises'][i]['user_input'].title()}',
             'duration': f'{nutritionix_response['exercises'][i]['duration_min']}',
             'calories': f'{round(nutritionix_response['exercises'][i]['nf_calories'])}',
@@ -61,16 +65,19 @@ for i in range(len(nutritionix_response['exercises'])):
     }
 
 
+    # Sheety Authorization Header
     sheety_header = {
         'Authorization': f'Bearer {API_KEY_SHEETY}',
     }
 
 
+    # Sheety response after adding to Google Sheets.
     sheety_post_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_params, headers=sheety_header)
     sheety_post_response.raise_for_status()
     print(sheety_post_response.text)
 
 
+    # DEBUG Statements
     #print(nutritionix_response['exercises'][i]['user_input'])
     #print(nutritionix_response['exercises'][i]['duration_min'])
     #print(nutritionix_response['exercises'][i]['nf_calories'])
