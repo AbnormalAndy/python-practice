f2041b94e10736b1562a05d18d28b33c8a3e9838
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-04-07 21:05:55 -0500

    Add code to add exercises to sheety.

--- 100DaysOfCodeProjects/day-38/main.py
+++ 100DaysOfCodeProjects/day-38/main.py
@@ -1,14 +1,25 @@
+from datetime import datetime
 import config
 import requests
 
 
+# API Configurations
 APP_ID = config.APP_ID
 API_KEY = config.API_KEY
 API_KEY_SHEETY = config.API_KEY_SHEETY
 
 
+# Endpoints
 EXERCISE_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 SHEETY_ENDPOINT = 'https://api.sheety.co/99857e1bc2a42bffc6f6a39865ac096e/myWorkouts/workouts'
+
+
+# Today's Date
+today = datetime.now()
+
+
+today_date = today.strftime('%m/%d/%Y')
+today_time = today.strftime('%H:%M:%S')
 
 
 # Input for exercise.
@@ -33,34 +44,35 @@ exercise_header = {
 }
 
 
-sheety_params = {
-    'workout': {
-        'date': 'meowdate',
-        'time': 'meowtime',
-        'exercise': 'meowexercise',
-        'duration': 'meowduration',
-        'calories': 'meowcalories',
+nutritionix_post_response = requests.post(url=EXERCISE_ENDPOINT, json=exercise_params, headers=exercise_header)
+nutritionix_post_response.raise_for_status()
+nutritionix_response = nutritionix_post_response.json()
+
+
+for i in range(len(nutritionix_response['exercises'])):
+    sheety_params = {
+        'workout': {
+            'date': f'{today_date}',
+            'time': f'{today_time}',
+            'exercise': f'{nutritionix_response['exercises'][i]['user_input'].title()}',
+            'duration': f'{nutritionix_response['exercises'][i]['duration_min']}',
+            'calories': f'{round(nutritionix_response['exercises'][i]['nf_calories'])}',
+        }
     }
-}
 
 
-sheety_header = {
-    'Authorization': f'Bearer {API_KEY_SHEETY}',
-}
+    sheety_header = {
+        'Authorization': f'Bearer {API_KEY_SHEETY}',
+    }
 
 
-#nutritionix_post_response = requests.post(url=EXERCISE_ENDPOINT, json=exercise_params, headers=exercise_header)
-#nutritionix_post_response.raise_for_status()
-#print(nutritionix_post_response.text)
+    sheety_post_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_params, headers=sheety_header)
+    sheety_post_response.raise_for_status()
+    print(sheety_post_response.text)
 
 
-#sheety_get_response = requests.get(url=SHEETY_ENDPOINT, headers=sheety_header)
-#sheety_get_response.raise_for_status()
-#print(sheety_get_response.text)
-
-
-sheety_post_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_params, headers=sheety_header)
-sheety_post_response.raise_for_status()
-print(sheety_post_response.text)
+    #print(nutritionix_response['exercises'][i]['user_input'])
+    #print(nutritionix_response['exercises'][i]['duration_min'])
+    #print(nutritionix_response['exercises'][i]['nf_calories'])
 
 
