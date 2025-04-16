674c41e63942b45de3df94abe347977def8b6f77
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-04-15 19:37:44 -0500

    Add files for Flight Deal Finder.

---
+++ 100DaysOfCodeProjects/day-39/main.py
@@ -0,0 +1,11 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+
+# Google Sheets
+# - Lowest Price
+# - Compare
+# Flight Search API
+# - In the next 6 months.
+# Twilio / Email
+
+
