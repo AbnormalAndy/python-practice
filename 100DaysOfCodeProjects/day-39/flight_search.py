fe42d7568e097364650efc907159aded89a7d7a0
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-04-16 17:47:09 -0500

    Add files for Flight Deal Finder.

--- 100DaysOfCodeProjects/day-39/flight_search.py
+++ 100DaysOfCodeProjects/day-39/flight_search.py
@@ -1,3 +1,9 @@
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     pass
+
+
+if __name__ == "__main__":
+    print("Meow")
+
+
--- 100DaysOfCodeProjects/day-39/main.py
+++ 100DaysOfCodeProjects/day-39/main.py
@@ -9,3 +9,4 @@
 # Twilio / Email
 
 
+
