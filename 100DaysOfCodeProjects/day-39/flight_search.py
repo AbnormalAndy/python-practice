d9b6fda96ff295820674b54079f264e5d0f1165a
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-04-20 13:07:54 -0500

    Add API calls to flight_search.py file.

--- 100DaysOfCodeProjects/day-39/flight_search.py
+++ 100DaysOfCodeProjects/day-39/flight_search.py
@@ -7,7 +7,9 @@ API_SECRET_AMADEUS = config.API_SECRET_AMADEUS
 API_TOKEN_AMADEUS = config.API_TOKEN_AMADEUS
 
 
-AMADEUS_ENDPOINT = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
+# Use dates to find cheapest price then offers to get further details?
+AMADEUS_DATES_ENDPOINT = 'https://test.api.amadeus.com/v1/shopping/flight-dates'
+AMADEUS_OFFERS_ENDPOINT = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
 
 
 AMADEUS_HEADER = {
@@ -20,22 +22,28 @@ class FlightSearch:
 
     # Tomorrow to 6 months from now?
     # - Will need to iterate through 180-ish dates for 9 cities?
-    amadeus_params = {
-        'originLocationCode': 'MSP',
-        'destinationLocationCode': 'PAR',
-        'departureDate': '2025-04-22',
-        'adults': 1,
-        'currencyCode': 'USD',
-        'maxPrice': 750,
+    amadeus_dates_params = {
+        'origin': 'MSP',
+        'destination': 'PAR',
+        'departureDate': '2025-04-23',
     }
 
 
-    amadeus_get_request = requests.get(url=AMADEUS_ENDPOINT, params=amadeus_params, headers=AMADEUS_HEADER)
+    amadeus_offers_params = {
+        'originLocationCode': 'MSP',
+        'destinationLocationCode': 'PAR',
+        'departureDate': '2025-04-23',
+        'adults': 1,
+    }
+
+
+    amadeus_get_request = requests.get(url=AMADEUS_DATES_ENDPOINT, params=amadeus_dates_params, headers=AMADEUS_HEADER)
     amadeus_get_request.raise_for_status()
-    response_amadeus = amadeus_get_request.json()
+    response_amadeus = amadeus_get_request.text
     # Figure out what information we need.
-    print(response_amadeus['data'][0]['price']['grandTotal'])
-    print(response_amadeus['data'][0]['itineraries'][0]['segments'])
+    print(response_amadeus)
+    #print(response_amadeus['data'][0]['price']['grandTotal'])
+    #print(response_amadeus['data'][0]['itineraries'][0]['segments'])
 
 
 if __name__ == "__main__":
